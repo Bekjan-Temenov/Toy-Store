dae9ff646ac804e23c11d102afb083f7467b1751
Bekjan <temenovbekzan@gmail.com>
2024-05-03 12:49:43 +0600

    products

--- src/Components/Products/Product.jsx
+++ src/Components/Products/Product.jsx
@@ -1,11 +1,13 @@
 import { Container } from "../../Container/Container";
 import Carausel from "../Products/Carausel"
+import Banners from "./Banners";
 import Input from "./Input";
 function Product() {
   return (
     <Container>
       <Carausel />
       <Input />
+      <Banners/>
     </Container>
   );
 }
